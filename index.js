a9316d915450c7cb6b60f76089a11ffa66b8cf15
younes <reguiegyounes@gmail.com>
2021-02-19 15:32:26 +0100

    commit connect with mongoDB

--- index.js
+++ index.js
@@ -1,14 +1,31 @@
-//Server generated
-var express = require('express');
-var app = express();
-
-
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+const app = express();
 app.use(express.json());
 
+//connect with mongoDB
+const uri =process.env.DB;
+mongoose.connect(uri,
+    {useNewUrlParser:true
+    ,useCreateIndex:true
+    , useUnifiedTopology:true
+    }
+)
+const connection =mongoose.connection;
+connection.once('open',()=>{
+    console.log("MongoDB database connection established successfuly")
+})
+
+
+//routes
 app.get('/', (req, res) => {
     return res.send('<h2>Welcome to Express App<h2>');
 })
 
+
+
+// run server
 var Port = process.env.PORT || 3000;
 var IP = process.env.IP || 'localhost';
 app.listen(Port, IP, (err) => {
@@ -18,3 +35,4 @@ app.listen(Port, IP, (err) => {
        console.log('Server is listening at ' + IP + ':' + Port);
     }
 });
+
