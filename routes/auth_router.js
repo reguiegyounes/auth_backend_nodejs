a843b06f09c89e537a745ec7e04a5bf901645cfe
younes <reguiegyounes@gmail.com>
2021-02-19 19:27:04 +0100

    commit checking if the user is already in the database

--- routes/auth_router.js
+++ routes/auth_router.js
@@ -11,6 +11,12 @@ router.post('/register',async(req,res)=>{
     if(error)
         return res.status(404).send(error.details[0].message);
 
+    //checking if the user is already in the database
+    const emailExist=await User.findOne({email:req.body.email});
+    if(emailExist)
+        return res.status(404).send('Email already exist');
+
+    // add user
     const user=new User({
         email:req.body.email,
         password:req.body.password
